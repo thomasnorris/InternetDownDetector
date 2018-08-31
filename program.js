d7dd44f0a59b79ac58572a5b99b3e569c6ff5a31
Thomas Norris <tom.norris135@gmail.com>
2018-08-31 12:21:41 -0400

    added more logic for detecting when the internet is down

--- program.js
+++ program.js
@@ -1,20 +1,32 @@
 (function() {
-    const CONSECUTIVE_FAILS = 20;
+    const CONSECUTIVE_FAILS = 10;
     const PING_TIME_MILLI = 1000;
-    var ping = require('ping').sys.probe;
-    var hosts = ['192.168.0.1', '8.8.8.8']
 
-    StartTimer(() => {
+    var ping = require('ping').sys.probe;
+    var hosts = ['8.8.8.8'];
+
+    var failCounter = 0;
+    var timer;
+    timer = StartTimer(() => {
         hosts.forEach((host) => {
             ping(host, (isAlive) => {
-                var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
-                console.log(msg);
+                if (!isAlive) {
+                    if (++failCounter == CONSECUTIVE_FAILS) {
+                        StopTimer(timer);
+                        console.log('The internet went down.');
+                    }
+                } else
+                    failCounter = 0;
             });
         });
-    }, PING_TIME_MILLI)
+    }, PING_TIME_MILLI);
 
 
     function StartTimer(functionToStart, loopTimeMilli) {
         return setInterval(functionToStart, loopTimeMilli);
+    }
+
+    function StopTimer(timer) {
+        clearInterval(timer);
     }
 })();
