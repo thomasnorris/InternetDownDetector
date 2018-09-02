5419d430811321080d41caca7b5542f2d780ddb1
Thomas Norris <tom.norris135@gmail.com>
2018-09-02 10:15:50 -0400

    successfully determines if the internet is down or not

--- program.js
+++ program.js
@@ -1,32 +1,31 @@
 (function() {
-    const CONSECUTIVE_FAILS = 10;
+    const CONSECUTIVE_COUNTER = 10;
     const PING_TIME_MILLI = 1000;
 
     var ping = require('ping').sys.probe;
     var hosts = ['8.8.8.8'];
 
     var failCounter = 0;
-    var timer;
-    timer = StartTimer(() => {
+    var succeedCounter = 0;
+    var down = false;
+    setInterval(() => {
         hosts.forEach((host) => {
             ping(host, (isAlive) => {
-                if (!isAlive) {
-                    if (++failCounter == CONSECUTIVE_FAILS) {
-                        StopTimer(timer);
+                if (!isAlive && !down) {
+                    if (++failCounter == CONSECUTIVE_COUNTER) {
+                        down = true;
                         console.log('The internet went down.');
                     }
-                } else
+                } else if (isAlive && down) {
+                    if (++succeedCounter == CONSECUTIVE_COUNTER) {
+                        down = false;
+                        console.log('The internet came back up.');
+                    }
+                } else {
                     failCounter = 0;
+                    succeedCounter = 0;
+                }
             });
         });
     }, PING_TIME_MILLI);
-
-
-    function StartTimer(functionToStart, loopTimeMilli) {
-        return setInterval(functionToStart, loopTimeMilli);
-    }
-
-    function StopTimer(timer) {
-        clearInterval(timer);
-    }
 })();
